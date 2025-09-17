843799c55cb1c446bc8d0bfddf0105ed91aa199e
juuanrebolledoo <otkneusky@gmail.com>
2025-09-17 21:34:55 +0200

    ejercicio-02

---
+++ ejercicio-02/src/Main.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    int entero = 2;
+    double decimal = 2.2;
+    boolean vf = true;
+    char caracter = 'a';
+    String palabra = "palabra";
+
+        System.out.println(entero);
+        System.out.println (decimal);
+        System.out.println(vf);
+        System.out.println( caracter);
+        System.out.println(palabra);
+    }
+
+
+}
