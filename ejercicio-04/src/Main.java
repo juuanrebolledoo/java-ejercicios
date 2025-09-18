9650ab424cadc2bbac77d33e97b8e46a6b6446b8
juuanrebolledoo <otkneusky@gmail.com>
2025-09-18 19:46:14 +0200

    ejercicio-04

---
+++ ejercicio-04/src/Main.java
@@ -0,0 +1,12 @@
+        
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 3 ;
+        int numero2 = 4 ;
+
+        int resultado = numero1 + numero2 ;
+
+        System.out.println("El resultado es: " + resultado );
+
+    }
+}
